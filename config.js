a4025771af06cf1490b3662c4dde8ad579099f6c
amenpunk <ondasycircuitos@gmail.com>
2022-06-09 18:06:47 -0600

    mecca?

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const CONFIG = {
     // └─┘┴ ┴└─┘┴└─┘└─┘
 
     // General
-    name: 'Ming',
+    name: 'Ming Mecca',
     imageBackground: false,
     openInNewTab: true,
     twelveHourFormat: false,
